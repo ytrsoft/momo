4a393fc348d2b07f6c447956d2099f66236fa50b
ytrsoft <1654486219@qq.com>
2024-12-15 20:08:02 +0800

    script

--- src/boot.py
+++ src/boot.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from momo import Momo
 
 momo = Momo(9388)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/profile/<id>', methods=['GET'])
 def profile(id):
