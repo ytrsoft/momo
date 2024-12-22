c69a5c2ec6733d6a52d622a7409f535b21e832f1
ytrsoft <1654486219@qq.com>
2024-12-22 08:46:07 +0800

    rpc

--- boot.py
+++ boot.py
@@ -5,7 +5,6 @@ from rpc import createRpc
 
 import json
 import requests
-import socketio
 
 momo = createRpc()
 
@@ -14,55 +13,61 @@ CORS(app)
 
 socket = SocketIO(app)
 
+def receive_msg(msg):
+    socket.send(msg)
+
+momo.exports.receiveMsg(receive_msg)
+
 @app.route('/profile/<id>', methods=['GET'])
 def profile(id):
-    result = momo.exports.profile(id)
+    result = momo.exports.fetchProfile(id)
     return jsonify(result)
 
 @app.route('/timeline/<id>', methods=['GET'])
 def timeline(id):
-    result = momo.exports.timeline(id)
+    result = momo.exports.fetchTimeline(id)
     return jsonify(result)
 
 @app.route('/comments/<id>', methods=['GET'])
 def comments(id):
-    result = momo.exports.comments(id)
+    result = momo.exports.fetchComments(id)
     return jsonify(result)
 
 @app.route('/news', methods=['GET'])
 def news():
-    result = momo.exports.news()
+    result = momo.exports.fetchNews()
     return jsonify(result)
 
 @app.route('/nearly', methods=['GET'])
 def nearly():
-    result = momo.exports.nearly()
+    result = momo.exports.fetchNearby()
     return jsonify(result)
 
 @app.route('/image/<id>', methods=['GET'])
 def image(id):
-    result = momo.exports.image(id)
+    result = momo.exports.fetchImage(id)
     image = requests.get(result, stream=True)
     return Response(image.content, content_type=image.headers['Content-Type'])
 
-@socketio.on('message')
+@socket.on('message')
 def ws_message(msg):
   data = json.loads(msg)
   momoid = data.get('momoid')
   remoteId = data.get('remoteId')
   content = data.get('content')
-  result = momo.exports.post({
+  msg = {
       'momoid': momoid,
       'remoteId': remoteId,
       'content': content
-  })
-  socketio.send(result)
+  }
+  result = momo.exports.postMsg(msg)
+  socket.send(result)
 
-@socketio.on('connect')
+@socket.on('connect')
 def ws_connect():
     print('Soccket开启连接')
 
-@socketio.on('disconnect')
+@socket.on('disconnect')
 def ws_disconnect():
     print('Soccket断开连接')
 
