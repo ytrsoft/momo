2de9c9df87e930ab88ac7baf26015c9a41b35a60
ytrsoft <1654486219@qq.com>
2024-12-24 19:07:24 +0800

    support process name

--- rpc.py
+++ rpc.py
@@ -1,14 +1,12 @@
 import frida
-from apply import get_pid
 
 def read_script(name):
     with open(f"{name}.js", 'r', encoding='utf8') as f:
         return f.read()
 
 def rpc():
-    pid = int(get_pid())
     device = frida.get_usb_device()
-    session = device.attach(pid)
+    session = device.attach('MOMO陌陌')
     code = read_script('rpc')
     script = session.create_script(code)
     script.load()
