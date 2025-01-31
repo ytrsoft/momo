abecd823dc8bab492f7b601a36ef23b350a1b144
ytrsoft <1654486219@qq.com>
2025-02-01 02:20:20 +0800

    fixed

--- unidbg-android/src/test/java/com/immomo/momo/KeyGen.java
+++ unidbg-android/src/test/java/com/immomo/momo/KeyGen.java
@@ -1,14 +1,15 @@
 package com.immomo.momo;
 
-import com.immomo.momo.emulator.CheckOS;
 import com.immomo.momo.emulator.ECDH;
+
 
 public class KeyGen {
 
     public static void main(String[] args) throws Exception {
-        CheckOS checkOS = ECDH.gen();
-        System.out.println("momo.key=" + checkOS.getSk());
-        System.out.println("momo.ck=" + checkOS.getCk());
-        System.out.println("momo.kv=" + checkOS.getKv());
+        ECDH.Result result = ECDH.gen();
+        String key = result.getKey();
+        String ck = result.getCk();
+        String kv = result.getKv();
+        System.out.println("momo.key=" + key + "\nmomo.ck=" + ck + "\nmomo.kv=" + kv);
     }
 }
--- unidbg-android/src/test/java/com/immomo/momo/emulator/ECDH.java
+++ unidbg-android/src/test/java/com/immomo/momo/emulator/ECDH.java
@@ -8,25 +8,62 @@ import java.util.Arrays;
 
 public final class ECDH {
 
+    private static final String KEY = "Iu0WKHFy";
+
     private ECDH() {
         throw new UnsupportedOperationException();
     }
 
-    public static CheckOS gen() {
-        Momo momo = new Momo();
-        SoCoded so = momo.loadCoded();
-        CheckOS checkOS = new CheckOS();
-        byte[] PK = so.getServerPK(2, 1);
-        ByteArray SK = so.newByteArray(24);
-        ByteArray CK = so.newByteArray(49);
-        so.clientSecretGen(SK, PK, PK.length, CK);
-        checkOS.setPk(Base64.encode(PK));
-        checkOS.setSk(Base64.encode(SK.getValue()));
-        checkOS.setCk(Base64.encode(CK.getValue()));
-        checkOS.setKv(Coded.md5(checkOS.getCk()).substring(0, 8));
-        momo.destroy();
-        return checkOS;
+    public static class Result {
+
+        private String key;
+        private String ck;
+        private String kv;
+
+        public void setCk(String ck) {
+            this.ck = ck;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public void setKv(String kv) {
+            this.kv = kv;
+        }
+
+        public String getCk() {
+            return ck;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getKv() {
+            return kv;
+        }
     }
 
+    public static Result gen() {
+        Momo momo = new Momo();
+        SoCoded so = momo.loadCoded();
+        byte[] PK = so.getServerPK(2, 1);
+        ByteArray SK_ARR = so.newByteArray(24);
+        ByteArray CK_ARR = so.newByteArray(49);
+        so.clientSecretGen(SK_ARR, PK, PK.length, CK_ARR);
+        byte[] SK = SK_ARR.getValue();
+        byte[] CK = CK_ARR.getValue();
+        byte[] encoded = Coded.encode(CK, KEY.getBytes());
+        String BASE64_CK = Base64.encode(encoded);
+        String KEY = Base64.encode(SK);
+        String KV = Coded.md5(BASE64_CK).substring(0, 8);
+        Result result = new Result();
+        result.setCk(BASE64_CK);
+        result.setKey(KEY);
+        result.setKv(KV);
+        momo.destroy();
+        return result;
+    }
 
 }
